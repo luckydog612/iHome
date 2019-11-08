13f1af3d9c6e7f95e29e32fae7667507de28ee0f
Jack Li <luckydog612@163.com>
2019-11-08 17:34:28 +0800

    添加前端文件，导入数据库

--- main.go
+++ main.go
@@ -1,11 +1,32 @@
 package main
 
 import (
-	_ "iHome/routers"
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context"
+	_ "iHome/models"
+	_ "iHome/routers"
+	"net/http"
+	"strings"
 )
 
 func main() {
+	ignoreStaticPath()
 	beego.Run()
 }
 
+// 忽略静态路径
+func ignoreStaticPath() {
+	// 透明Static
+	beego.InsertFilter("/", beego.BeforeRouter, transparentStatic)
+	beego.InsertFilter("/*", beego.BeforeRouter, transparentStatic)
+}
+
+func transparentStatic(ctx *context.Context) {
+	path := ctx.Request.URL.Path
+	beego.Debug("request url: ", path)
+
+	if strings.Index(path, "api") >= 0 {
+		return
+	}
+	http.ServeFile(ctx.ResponseWriter, ctx.Request, "static/html/"+ctx.Request.URL.Path)
+}
