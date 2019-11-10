4373efd5b8f78085e030b4310f96f0e9fca8b922
Jack Li <luckydog612@163.com>
2019-11-11 05:41:57 +0800

    添加登录功能

--- controllers/session.go
+++ controllers/session.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
 	. "iHome/models"
 )
 
@@ -41,6 +42,51 @@ func (c *SessionController) DeleteSessionData() {
 	var resp = make(map[string]interface{})
 	defer c.ReturnData(resp)
 	c.DelSession("name")
+	resp["errno"] = RECODE_OK
+	resp["errmsg"] = RecodeText(RECODE_OK)
+}
+
+func (c *SessionController) Login() {
+	var resp = make(map[string]interface{})
+	defer c.ReturnData(resp)
+
+	// 1.获取传回数据
+	var req = make(map[string]interface{})
+	err := json.Unmarshal(c.Ctx.Input.RequestBody, &req)
+	if err != nil {
+		resp["errno"] = RECODE_REQERR
+		resp["errmsg"] = RecodeText(RECODE_REQERR)
+		return
+	}
+	userName := req["mobile"].(string)
+	password := req["password"].(string)
+	// 2.校验数据
+	if userName == "" || password == "" {
+		resp["errno"] = RECODE_REQERR
+		resp["errmsg"] = RecodeText(RECODE_REQERR)
+		return
+	}
+	user := User{Name: userName}
+	o := orm.NewOrm()
+	err = o.QueryTable("user").Filter("name", user.Name).One(&user)
+	if err != nil {
+		beego.Error("login query db err: ", err)
+		resp["errno"] = RECODE_DBERR
+		resp["errmsg"] = RecodeText(RECODE_DBERR)
+		return
+	}
+
+	if user.Password_hash != password {
+		resp["errno"] = RECODE_DATAERR
+		resp["errmsg"] = RecodeText(RECODE_DATAERR)
+		return
+	}
+	// 3.设置session
+	c.SetSession("name", user.Name)
+	c.SetSession("mobile", user.Mobile)
+	c.SetSession("user_id", user.Id)
+
+	// 4.返回数据
 	resp["errno"] = RECODE_OK
 	resp["errmsg"] = RecodeText(RECODE_OK)
 }
